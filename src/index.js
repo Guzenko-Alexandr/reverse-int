7c3bf86168fc547502841ced4b3fb5787da1aa02
Guzenko-Alexandr <sasha5539107@mail.ru>
2021-03-14 19:03:33 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+   let nstr = n.toString().split("").reverse().join("")
+   let ansv = parseInt(nstr);
+   return ansv;
 }
+
+
+// reverse(123); // 321
+// reverse(233); // 332
+// reverse(535); // 535
+// reverse(95034); // 43059
